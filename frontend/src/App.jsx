7ee0d678a3931e33c3a6f571e28e2a5d76d836f8
Jatin Rajput <jatinrjpt21@gmail.com>
2025-04-15 11:46:26 +0530

    notFound page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Home from './pages/Home.jsx'
+import { Route, Switch } from 'wouter'
 
 const App = () => {
   return (
     <div>
       {/* <h1 className='text-red-400'>App</h1> */}
-      <Home/>
+      <Switch>
+      <Route path="/" component={Home} />
+
+     
+      {/* Optionally a fallback route */}
+      <Route>
+        <div className="text-center mt-20">
+          <h1>404 - Page Not Found</h1>
+        </div>
+      </Route>
+      </Switch>
     </div>
   )
 }
