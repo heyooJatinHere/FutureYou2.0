e063d093ebe3383b77c41ea3ea0c20288afac4d3
heyoojatinhere <itzrj216@gmail.com>
2025-04-19 12:12:25 +0530

    added '/' route in questions

--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -3,6 +3,11 @@ import express from 'express';
 import fs from 'fs';
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.send("Questions route working");
+});
+
+
 router.get('/health', (req, res) => {
   const data = JSON.parse(fs.readFileSync('./data/health.json', 'utf-8'));
   res.json(data);
