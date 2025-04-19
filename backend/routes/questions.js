51dbdc57a89e246849109e3fecf8cda53b19816f
heyoojatinhere <itzrj216@gmail.com>
2025-04-19 11:45:58 +0530

    updated vercel.json

--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -1,10 +1,14 @@
 // routes/questions.js
 import express from 'express';
 import fs from 'fs';
+import path from 'path';
 const router = express.Router();
 
 router.get('/health', (req, res) => {
-  const data = JSON.parse(fs.readFileSync('./data/health.json', 'utf-8'));
+  
+const dataPath = path.resolve(process.cwd(), 'data', 'health.json');
+const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+
   res.json(data);
 });
 
